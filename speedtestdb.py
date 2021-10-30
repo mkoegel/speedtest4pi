7c339e0e5df2f378246edf3b2fcd0437f45c4788
Michael Koegel <34140+mkoegel@users.noreply.github.com>
2021-10-30 10:42:48 +0200

    Changed to new library and switched to JSON processing

--- speedtestdb.py
+++ speedtestdb.py
@@ -1,5 +1,6 @@
 import os
-import re
+import json
+from types import SimpleNamespace
 import subprocess
 import time
 import sqlite3
@@ -37,16 +38,24 @@ def main():
     print(path)
     database = rf"{path}/speedtest.db"
 
-    response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read()
+    response = subprocess.Popen('/usr/bin/speedtest -f json', shell=True, stdout=subprocess.PIPE).stdout.read()
     response = response.decode('UTF-8')
     print(response)
 
-    regex = 'Ping:\s(?P<ping>\d*\.?\d*).*\s^Download:\s(?P<download>\d*\.?\d*).*\s^Upload:\s(?P<upload>\d*\.?\d*)'
-    matches = re.search(regex, response, re.MULTILINE)
+    # regex = 'Ping:\s(?P<ping>\d*\.?\d*).*\s^Download:\s(?P<download>\d*\.?\d*).*\s^Upload:\s(?P<upload>\d*\.?\d*)'
+    # matches = re.search(regex, response, re.MULTILINE)
     
-    ping = matches.group("ping")
-    download = matches.group("download")
-    upload = matches.group("upload")
+    # ping = matches.group("ping")
+    # download = matches.group("download")
+    # upload = matches.group("upload")
+
+    data = json.loads(response, object_hook=lambda d: SimpleNamespace(**d))
+    ping = data.ping.latency
+    download = data.download.bandwidth
+    upload = data.upload.bandwidth
+    print(f"Ping: {ping}")
+    print(f"Download: {download}")
+    print(f"Upload: {upload}")
 
     # create a database connection
     conn = create_connection(database)
